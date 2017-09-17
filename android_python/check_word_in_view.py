ea3e63ff284302e523fa63b90262b35c9c276f04
ki4070ma <ki4070ma@gmail.com>
2017-09-17 23:36:45 +0900

    [Python] Added check_focused_word method

--- android_python/check_word_in_view.py
+++ android_python/check_word_in_view.py
@@ -1,27 +1,36 @@
+import sys
+
+import xml.etree.ElementTree as ET
 
 def check_word_in_view(word):
-  import xml.etree.ElementTree as ET
   root = ET.fromstring(dump_ui())
   for node in root.iter('node'):
     text = node.attrib['text']
-    # if text:
+    # if text:  # For debug
     #   print text
     if word in text:
       return True
   return False
 
+def check_focused_word(word):
+  root = ET.fromstring(dump_ui())
+  for node in root.findall('.//node[@focused="true"]'):
+    for n in node.iter('node'):
+      if word in n.attrib['text']:
+        return True
+  return False
+
 def dump_ui():
-  # xml = ''  # For Debug
-  # with open('dump.txt', 'r') as fr:
-  #  xml = fr. read()
+  # with open('dump.txt', 'r') as fr:  # For Debug
+  #  return fr. read()
   import subprocess as sp
   return sp.check_output('adb shell uiautomator dump | adb shell cat /sdcard/window_dump.xml | tidy -xml -utf8 -i 2>/dev/null', shell=True).strip()
 
 if __name__ == '__main__':
-  import sys
   argvs = sys.argv
   argc = len(argvs)
   if argc != 2:
     print '****Usage: # python {} word'.format(argvs[0])
     sys.exit(1)
   print check_word_in_view(argvs[1])
+  # print check_focused_word(argvs[1])
