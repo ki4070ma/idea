a3503791956fde84892a09997d83d0e7d40fb366
ki4070ma <ki4070ma@gmail.com>
2017-09-18 10:29:41 +0900

    [Python] Added apis to launch app and capture screenshot

--- android_python/controller.py
+++ android_python/controller.py
@@ -12,6 +12,18 @@ class Controller(object):
     for step in steps:
       sp.check_output('adb shell input keyevent DPAD_{}'.format(step).split())
 
+  def start_settings(self):
+    sp.call('adb shell am start -n com.android.settings/.Settings'.split())
+
+  def am_start(self, package):
+    sp.call('adb shell am start -n {}'.format(package).split())
+
+  def cap(self, str=''):
+    from time import localtime, strftime
+    sp.call('adb shell screencap -p /sdcard/screen.png'.split())
+    sp.call('adb pull /sdcard/screen.png ./screen_{}_{}.png'.format(strftime("%Y%m%d%H%M%S", localtime()), str).split())
+    sp.call('adb shell rm /sdcard/screen.png'.split())
+
   def __check_steps(self, steps):
     for cmd in steps:
       if cmd not in self.CMD:
@@ -20,3 +32,6 @@ class Controller(object):
 if __name__ == '__main__':
   controller = Controller()
   controller.execute(['DOWN', 'DOWN', 'DOWN'])
+  # controller.start_settings()
+  # controller.am_start('com.android.settings/.Settings')
+  # controller.cap('Settings')
